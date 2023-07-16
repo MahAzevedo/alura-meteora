64c33644bd934e5dcca0ff6e33a84971c8475d18
Mah <madavetto@gmail.com>
2023-07-16 00:25:44 -0300

    new file

---
+++ js/menux.js
@@ -0,0 +1,14 @@
+function myFunction(x) {
+    x.classList.toggle('change');
+}
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
